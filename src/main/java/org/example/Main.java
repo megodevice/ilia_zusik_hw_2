d66e4e2424b9f4dc54621897cf6df461f98ed9f2
megodevice <megodevice14@gmail.com>
2023-10-02 23:19:28 +0600

    ilia_zusik_hw_2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkForWalk(22, 0));
+        System.out.println(checkForWalk(20, 10));
+        System.out.println(checkForWalk(18, 17));
+        System.out.println(checkForWalk(51, -3));
+        System.out.println(checkForWalk(12, -1));
+        System.out.println(checkForWalk(generateRandomAge(), -1));
+    }
+
+    public static String checkForWalk(int age, int temp) {
+        String youCanGo = "Можно идти гулять";
+        String youCantGo = "Оставайтесь дома";
+
+        if ((age >= 20 && age <= 45) && (temp >= -20 && temp <= 30)) {
+            return youCanGo;
+        } else if (age < 20 && (temp >= 0 && temp <= 28)) {
+            return youCanGo;
+        } else if (age > 45 && (temp >= -10 && temp <= 25)) {
+            return youCanGo;
+        } else {
+            return youCantGo;
+        }
+    }
+
+    public static int generateRandomAge() {
+        Random rand = new Random();
+        return rand.nextInt(100); // age from 0 to 100
+    }
+}
